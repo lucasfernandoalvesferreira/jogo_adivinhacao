cfd154ead9cbea7eca7c87c4101620b282f55e41
Lucas Fernando Alves Ferreira <37386521+lfaferreira@users.noreply.github.com>
2022-02-16 20:49:23 -0300

    Modifica saida de informacao pro usuario

--- 01 - Intodrução Python/jogo_adivinhacao/adivinhacao.py
+++ 01 - Intodrução Python/jogo_adivinhacao/adivinhacao.py
@@ -7,8 +7,17 @@ numero_secreto = 42
 chute_str = input("Digite o seu número: ")
 print('Você digitou: ', chute_str)
 chute = int(chute_str)
-if (numero_secreto == chute):
+
+acertou = numero_secreto == chute
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
+if (acertou):
     print('Você acertou!')
 else:
-    print('Você errou!')
+    if (maior):
+        print('Você errou! O seu chute foi maior do que o número secreto.')
+    elif (menor):
+        print('Você errou! O seu chute foi menor do que o número secreto.')
+
 print('Fim do jogo')
