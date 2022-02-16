bd9c461529bac219adc9d53a20b720c795709137
Lucas Fernando Alves Ferreira <37386521+lfaferreira@users.noreply.github.com>
2022-02-16 20:52:24 -0300

    Adiciona repetição no game e melhora formatação de mensagem

--- 01 - Intodrução Python/jogo_adivinhacao/adivinhacao.py
+++ 01 - Intodrução Python/jogo_adivinhacao/adivinhacao.py
@@ -3,21 +3,27 @@ print('Bem vindo ao jogo de Adivinhação!')
 print('*********************************')
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute_str = input("Digite o seu número: ")
-print('Você digitou: ', chute_str)
-chute = int(chute_str)
+while(total_de_tentativas >= rodada):
+    print(f'Tentativa {rodada} de {total_de_tentativas}')
 
-acertou = numero_secreto == chute
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    chute_str = input("Digite o seu número: ")
+    print(f'Você digitou: {chute_str}')
+    chute = int(chute_str)
 
-if (acertou):
-    print('Você acertou!')
-else:
-    if (maior):
-        print('Você errou! O seu chute foi maior do que o número secreto.')
-    elif (menor):
-        print('Você errou! O seu chute foi menor do que o número secreto.')
+    acertou = numero_secreto == chute
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if (acertou):
+        print('Você acertou!')
+    else:
+        if (maior):
+            print('Você errou! O seu chute foi maior do que o número secreto.')
+        elif (menor):
+            print('Você errou! O seu chute foi menor do que o número secreto.')
+    rodada = rodada + 1
 
 print('Fim do jogo')
